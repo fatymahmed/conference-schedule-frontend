19bb4ded8d5b689f13f762ed34ff86914c11f5f5
fatima ahmed <fatymah001@gmail.com>
2019-12-11 00:17:20 +0300

    Add fetch methods for post,get and delete

--- src/components/talk.js
+++ src/components/talk.js
@@ -6,6 +6,8 @@ const Talk = (props) => {
   return (
     <div>
       <h1>{talk.title}</h1>
+      {/* DateTime.new(2011,10,1,9).to_s(:time) #=> "09:00" */}
+      <button>Add to schedule</button>
     </div>
   )
 };
---
+++ src/services/api-service.js
@@ -0,0 +1,46 @@
+const get = (onSuccess, onError, apiURL) => {
+  fetch(apiURL, {method: 'GET'}).then(result => result.json())
+  .then((result) => {
+    if (result.error) {
+      onError();
+    }
+    else {
+      onSuccess(result);
+    }
+  }).catch(error => onError(error));
+};
+
+const post = ( onSuccess, onError, data, apiURL) => {
+  fetch(apiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+    body: JSON.stringify(data),
+  }).then(result => result.json())
+  .then((result) => {
+    if (result.error) {
+      onError();
+    } else {
+      onSuccess(result);
+    }
+  }).catch(error => onError(error));
+};
+
+const remove = (onSuccess, onError, apiURL) => {
+  fetch(apiURL, {method: 'DELETE'}).then(result => result.json())
+  .then((result) => {
+    if (result.error) {
+      onError();
+    }
+    else {
+      onSuccess(result);
+    }
+  }).catch(error => onError(error));
+};
+
+export {
+  post,
+  get,
+  remove,
+}
