df47554ae503b77ae94c8f53458ac5f9814a90b4
fatima ahmed <fatymah001@gmail.com>
2019-12-10 10:33:55 +0300

    Add actions for adding and removing talks

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+const ADD_TALKS = 'add_talks';
+const REMOVE_TALK = 'remove_talk';
+const PROCESSING = 'processing';
+const FETCH_SUCCESS = 'fetch_success';
+const FETCH_ERROR = 'fetch_error';
+
+const addTalks = talks => ({
+  type: ADD_TALKS,  
+  talks,
+})
+
+const removeTalk = id => ({
+  type: REMOVE_TALK,
+  id,
+})
+
