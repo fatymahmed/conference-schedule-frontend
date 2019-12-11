60711c033c7bf3c0c0693daa83496bc686fc85df
fatima ahmed <fatymah001@gmail.com>
2019-12-12 01:01:23 +0300

    change page to dashboard from home once user logs in

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,19 @@ import Registration from './auth/Registration';
 export default class Home extends React.Component {
   constructor(props){
     super(props);
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+  }
+
+  handleSuccessfulAuth(data) {
+    //UPDATE PARENT COMPONENT
+    this.props.history.push('/dashboard');
   }
   render() {
     return(
       <div>
         <h1>Home</h1>
         <h1> Status {this.props.loggedInStatus}</h1>
-        <Registration />
+        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth}/>
       </div>
     )
   }
