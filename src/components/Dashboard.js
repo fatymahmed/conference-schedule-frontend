983e1cded7e141ce1492d438d9794f2bb6a20f1e
fatima ahmed <fatymah001@gmail.com>
2019-12-11 23:22:10 +0300

    Add Dashboard and Home components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return(
+      <div>
+          <div>
+              <h1>Dashboard</h1>
+          </div>
+          </div>
+  )
+}
+export default Dashboard;
